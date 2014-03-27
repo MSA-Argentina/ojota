adbafc14ff8d6c891cc326a82a029096b9ce1955
Felipe Lerena <felipelerena@gmail.com>
2014-03-27 17:12:02 -0300

    updating setup

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 
 setup(
     name='Ojota',
-    version='1.0.0',
+    version='1.1.0',
     author='Felipe Lerena, Juan Pedro Fisanoti, Ezequiel Chan, Nicolas Sarubbi, Hernan Lozano, Nicolas Bases',
     author_email='flerena@msa.com.ar, fisadev@gmail.com, echan@msa.com.ar, nicosarubi@gmail.com, hernantz@gmail.com, nmbases@gmai.com',
     packages=['ojota'],
